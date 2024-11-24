8ebf504c77390e249eb83948a59f73be4cdbd802
bruizk <beruizk@gmail.com>
2024-11-24 23:39:50 +0100

    Send to Telegram every 30 minutes the information about coins added in the last half-hour.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='PluteosX-sybil-sanctum',
-    version='1.1.0',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=required,
 )
--- src/main.py
+++ src/main.py
@@ -4,19 +4,20 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.constants import CHAINS_INFO_TXT, TOP_10_WEEK_COINS_TXT, TODAY_COINS_TXT, CHAINS_INFO, TOP_10_WEEK_COINS, \
-    TODAY_COINS
-from src.utils.file_utils import chain_info_txt_file, top_10_coins_txt_file, today_coins_txt_file
-from src.utils.telegram_utils import send_message
-from src.utils.analytics_utils import get_chain_info, get_top_10_coin_info, get_week_coins_df
+from constants import CHAINS_INFO_TXT, TOP_10_WEEK_COINS_TXT, TODAY_COINS_TXT, CHAINS_INFO, TOP_10_WEEK_COINS, \
+    TODAY_COINS, LAST_HOUR_COINS
+from utils.analytics_utils import get_chain_info, get_top_10_coin_info, get_week_coins_df
+from utils.coin_utils import get_week_market_data_coins, get_added_coins
 from utils.coingecko_utils import get_new_cryptocurrencies_list
-from utils.coin_utils import get_week_market_data_coins, get_today_added_coins
+from utils.file_utils import chain_info_txt_file, top_10_coins_txt_file, today_coins_txt_file
+from utils.message_utils import send_last_hour_coins_message
+from utils.telegram_utils import send_message_file
 
 
 def main():
     parser = argparse.ArgumentParser(description="Generate specific cryptocurrency reports")
-    parser.add_argument("--generate", choices=[CHAINS_INFO, TOP_10_WEEK_COINS, TODAY_COINS], required=True,
-                        help="Specify which file to generate")
+    parser.add_argument("--generate", choices=[CHAINS_INFO, TOP_10_WEEK_COINS, TODAY_COINS, LAST_HOUR_COINS],
+                        required=True, help="Specify which file to generate")
     args = parser.parse_args()
 
     new_cryptocurrencies_list = get_new_cryptocurrencies_list()
@@ -26,15 +27,19 @@ def main():
     if args.generate == CHAINS_INFO:
         df_chain_info = get_chain_info(df_week_coins)
         chain_info_txt_file(df_chain_info.to_dict(orient='records'))
-        send_message(CHAINS_INFO_TXT)
+        send_message_file(CHAINS_INFO_TXT)
     elif args.generate == TOP_10_WEEK_COINS:
         df_top_10_coins = get_top_10_coin_info(df_week_coins)
         top_10_coins_txt_file(df_top_10_coins.to_dict(orient='records'))
-        send_message(TOP_10_WEEK_COINS_TXT)
+        send_message_file(TOP_10_WEEK_COINS_TXT)
     elif args.generate == TODAY_COINS:
-        today_coins = get_today_added_coins(new_cryptocurrencies_list)
+        today_coins = get_added_coins(new_cryptocurrencies_list, TODAY_COINS)
         today_coins_txt_file(today_coins)
-        send_message(TODAY_COINS_TXT)
+        send_message_file(TODAY_COINS_TXT)
+    elif args.generate == LAST_HOUR_COINS:
+        new_cryptocurrencies_list = get_new_cryptocurrencies_list()
+        last_hour_coins = get_added_coins(new_cryptocurrencies_list, LAST_HOUR_COINS)
+        send_last_hour_coins_message(last_hour_coins)
 
 
 if __name__ == "__main__":
